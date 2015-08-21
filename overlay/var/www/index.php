45480208ff4d39f7ca029d9fa1bfc1e9e7f4c320
Jeremy Davis <jeremy@turnkeylinux.org>
2015-08-21 02:36:49 +0000

    tweaked landing page (tkl-webcp) for fileserver

--- overlay/var/www/index.php
+++ overlay/var/www/index.php
@@ -8,8 +8,8 @@
 
         <title>TurnKey File Server</title>
         
-        <link rel="stylesheet" href="css/ui.tabs.css" type="text/css" media="print, projection, screen">
-        <link rel="stylesheet" href="css/base.css" type="text/css">
+        <link rel="stylesheet" href="css/ui.tabs.css" type="text/css" media="print, projection, screen"/>
+        <link rel="stylesheet" href="css/base.css" type="text/css"/>
 
         <script src="js/jquery-1.2.6.js" type="text/javascript"></script>
         <script src="js/ui.core.js" type="text/javascript"></script>
@@ -34,36 +34,39 @@
                     <div>
                         <a href="https://<?php print
                         $_SERVER{'HTTP_HOST'}; ?>:12321/samba/index.cgi">
-                        <img src="images/webmin.png"/>Samba<br/>(via webmin)</a>
+                        <img src="images/samba.png"/>Samba<br/>(via webmin)</a>
+                    </div>             
+                    <div>
+                        <a href="https://<?php print
+                        $_SERVER{'HTTP_HOST'}; ?>:12320"><img
+                        src="images/shell.png"/>Web Shell</a>
                     </div>
                     <div>
                         <a href="https://<?php print
-                        $_SERVER{'HTTP_HOST'}; ?>:12320">
-                        <img src="images/shell.png"/>Web Shell</a>
+                        $_SERVER{'HTTP_HOST'}; ?>:12321"><img
+                        src="images/webmin.png"/>Webmin</a>
                     </div>
                     <div>
                         <a href="https://<?php print
-                        $_SERVER{'HTTP_HOST'}; ?>:12321">
-                        <img src="images/webmin.png"/>Webmin</a>
+                        $_SERVER{'HTTP_HOST'}; ?>:443"><img
+                        src="images/filemanager.png"/>File Manager<br/>(WebDAV)</a>
                     </div>
-                    <div>
-                        <a href="https://<?php print
-                        $_SERVER{'HTTP_HOST'}; ?>:443">
-                        <img src="images/filemanager.png"/>File Manager<br/>(WebDAV)</a>
-                    </div>
+                    <div></div>
 
                     <h2>Resources and references</h2>
                     <ul>
-                        <li>Samba documentation (<a href="/samba">local</a>,
-                        <a href="http://samba.org/samba/docs">online</a>)</li>
-
-                        <li><a href="http://www.turnkeylinux.org/fileserver">TurnKey File Server release notes</a></li>
+                        <li>
+                           <a href="http://samba.org/samba/docs">
+                           Samba documentation</a>
+                        </li>
+                        <li>
+                          <a href="http://www.turnkeylinux.org/fileserver">
+                          TurnKey File Server release notes</a>
+                        </li>
                     </ul>
-
                 </div>
             </div>
 
         </div>
     </body>
 </html>
-
