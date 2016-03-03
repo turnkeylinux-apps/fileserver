218d3182fc278f4c9058465d147839d84adf5c90
Anton Pyrogovskyi <anton@turnkeylinux.org>
2016-03-03 11:48:43 +0000

    Replace tklwebcp index.php with a Perl CGI version so we don't pull in PHP

--- overlay/var/www/cgi-bin/index.pl
+++ overlay/var/www/cgi-bin/index.pl
@@ -1,3 +1,9 @@
+#!/usr/bin/perl
+
+print <<EOF;
+Content-Type: text/html; charset=utf-8
+
+
 <!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01//EN" "http://www.w3.org/TR/html4/strict.dtd">
 
 <html lang="en">
@@ -7,7 +13,7 @@
         <meta http-equiv="Content-Script-Type" content="text/javascript">
 
         <title>TurnKey File Server</title>
-        
+
         <link rel="stylesheet" href="css/ui.tabs.css" type="text/css" media="print, projection, screen"/>
         <link rel="stylesheet" href="css/base.css" type="text/css"/>
 
@@ -15,15 +21,15 @@
         <script src="js/ui.core.js" type="text/javascript"></script>
         <script src="js/ui.tabs.js" type="text/javascript"></script>
         <script type="text/javascript">
-            $(function() {
-                $('#container-1 > ul').tabs({ fx: { opacity: 'toggle'} });
+            \$(function() {
+                \$('#container-1 > ul').tabs({ fx: { opacity: 'toggle'} });
             });
         </script>
     </head>
 
     <body>
         <h1>TurnKey File Server</h1>
-        
+
         <div id="container-1">
             <ul>
                 <li><a href="#cp"><span>Control Panel</span></a></li>
@@ -32,23 +38,19 @@
             <div id="cp">
                 <div class="fragment-content">
                     <div>
-                        <a href="https://<?php print
-                        $_SERVER{'HTTP_HOST'}; ?>:12321/samba/index.cgi">
+                        <a href="https://$ENV{HTTP_HOST}:12321/samba/index.cgi">
                         <img src="images/samba.png"/>Samba<br/>(via webmin)</a>
-                    </div>             
+                    </div>
                     <div>
-                        <a href="https://<?php print
-                        $_SERVER{'HTTP_HOST'}; ?>:12320"><img
+                        <a href="https://$ENV{HTTP_HOST}:12320"><img
                         src="images/shell.png"/>Web Shell</a>
                     </div>
                     <div>
-                        <a href="https://<?php print
-                        $_SERVER{'HTTP_HOST'}; ?>:12321"><img
+                        <a href="https://$ENV{HTTP_HOST}:12321"><img
                         src="images/webmin.png"/>Webmin</a>
                     </div>
                     <div>
-                        <a href="https://<?php print
-                        $_SERVER{'HTTP_HOST'}; ?>:443"><img
+                        <a href="https://$ENV{HTTP_HOST}/"><img
                         src="images/filemanager.png"/>File Manager<br/>(WebDAV)</a>
                     </div>
                     <div></div>
@@ -66,7 +68,8 @@
                     </ul>
                 </div>
             </div>
-
         </div>
     </body>
 </html>
+EOF
+
